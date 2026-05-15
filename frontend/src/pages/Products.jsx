563075502d81fcbae6bd69de45380665f111afb9
Elliott Miller <elliotttmiller@hotmail.com>
2026-05-15 09:37:55 -0500

    Use catalog platform as products runtime

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,7 +1,5 @@
-import LegacyProducts from './LegacyProducts.jsx';
 import ProductsCatalogPlatform from './ProductsCatalogPlatform.jsx';
-import { isCatalogPlatformEnabled } from '../utils/featureFlags.js';
 
 export default function Products() {
-  return isCatalogPlatformEnabled() ? <ProductsCatalogPlatform /> : <LegacyProducts />;
+  return <ProductsCatalogPlatform />;
 }
