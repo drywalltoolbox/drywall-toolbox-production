8889cc705e2fd1becc2304f1393dde69d305d4e1
Elliott Miller <elliotttmiller@hotmail.com>
2026-05-15 11:11:17 -0500

    Allow products route mode props

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,5 +1,5 @@
 import ProductsCatalogPlatform from './ProductsCatalogPlatform.jsx';
 
-export default function Products() {
-  return <ProductsCatalogPlatform />;
+export default function Products(props) {
+  return <ProductsCatalogPlatform {...props} />;
 }
