8f1240496cc0e1bcb2264a96df792e9e2eab6739
Elliott Miller <elliotttmiller@hotmail.com>
2026-06-16 00:26:00 -0500

    Load product card performance overrides with product cards

--- frontend/src/components/ui/ProductShoppingCard.jsx
+++ frontend/src/components/ui/ProductShoppingCard.jsx
@@ -1,4 +1,5 @@
 import StorefrontProductTile from '../storefront/StorefrontProductTile';
+import '../../styles/performance-overrides.css';
 
 export default function ProductShoppingCard(props) {
   return <StorefrontProductTile {...props} />;
