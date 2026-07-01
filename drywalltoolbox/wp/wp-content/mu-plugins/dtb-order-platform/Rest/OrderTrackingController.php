246f32815c1faa33f9a1c7d68f2f667660b73691
Elliott Miller <elliotttmiller@hotmail.com>
2026-07-01 01:36:30 -0500

    Block tracking for incomplete payment orders

--- drywalltoolbox/wp/wp-content/mu-plugins/dtb-order-platform/Rest/OrderTrackingController.php
+++ drywalltoolbox/wp/wp-content/mu-plugins/dtb-order-platform/Rest/OrderTrackingController.php
@@ -8,7 +8,13 @@
 defined( 'ABSPATH' ) || exit;
 
 function dtb_order_rest_get_tracking( WP_REST_Request $request ): WP_REST_Response|WP_Error {
-	$order_id   = (int) $request->get_param( 'id' );
+	$order_id = (int) $request->get_param( 'id' );
+	$order    = function_exists( 'wc_get_order' ) ? wc_get_order( $order_id ) : null;
+
+	if ( $order instanceof WC_Order && function_exists( 'dtb_payment_is_incomplete_checkout_order' ) && dtb_payment_is_incomplete_checkout_order( $order ) ) {
+		return new WP_Error( 'dtb_not_found', 'Order not found.', [ 'status' => 404 ] );
+	}
+
 	$projection = dtb_order_get_tracking_projection( $order_id );
 
 	if ( null === $projection ) {
