eb0b499005e0673bbb6d8cd3f0cfbebb35e1fb6e
Elliott Miller <elliotttmiller@hotmail.com>
2026-07-03 13:09:17 -0500

    Load coming soon referral capture

--- drywalltoolbox/wp/wp-content/mu-plugins/dtb-marketing/bootstrap.php
+++ drywalltoolbox/wp/wp-content/mu-plugins/dtb-marketing/bootstrap.php
@@ -8,4 +8,5 @@
 defined( 'ABSPATH' ) || exit;
 
 dtb_module_require( 'dtb-marketing/ComingSoon/ComingSoonController.php' );
+dtb_module_require( 'dtb-marketing/ComingSoon/ComingSoonReferral.php' );
 dtb_module_require( 'dtb-marketing/Seo/ProductSeoController.php' );
