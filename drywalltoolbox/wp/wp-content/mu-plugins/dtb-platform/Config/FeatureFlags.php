3c1cf818592039ad2ade85c897d0039caea60102
Elliott Miller <elliotttmiller@hotmail.com>
2026-07-13 23:42:44 -0500

    Default Woo admin compatibility shims off

--- drywalltoolbox/wp/wp-content/mu-plugins/dtb-platform/Config/FeatureFlags.php
+++ drywalltoolbox/wp/wp-content/mu-plugins/dtb-platform/Config/FeatureFlags.php
@@ -12,13 +12,15 @@ final class DTB_FeatureFlags {
 
 	public static function all(): array {
 		return [
-			'DTB_ENABLE_REST_CORS'         => self::enabled( 'DTB_ENABLE_REST_CORS', true ),
-			'DTB_RESTRICT_USER_ENDPOINTS'  => self::enabled( 'DTB_RESTRICT_USER_ENDPOINTS', true ),
-			'DTB_WC_PUBLIC_READ'           => self::enabled( 'DTB_WC_PUBLIC_READ', true ),
-			'DTB_ENABLE_NONCE_REFRESH'     => self::enabled( 'DTB_ENABLE_NONCE_REFRESH', true ),
-			'DTB_SECURITY_LOGGING'         => self::enabled( 'DTB_SECURITY_LOGGING', true ),
-			'DTB_ENABLE_PROXY_RATE_LIMIT'  => self::enabled( 'DTB_ENABLE_PROXY_RATE_LIMIT', true ),
-			'DTB_ENABLE_LOGIN_RATE_LIMIT'  => self::enabled( 'DTB_ENABLE_LOGIN_RATE_LIMIT', true ),
+			'DTB_ENABLE_REST_CORS'                    => self::enabled( 'DTB_ENABLE_REST_CORS', true ),
+			'DTB_RESTRICT_USER_ENDPOINTS'             => self::enabled( 'DTB_RESTRICT_USER_ENDPOINTS', true ),
+			'DTB_WC_PUBLIC_READ'                      => self::enabled( 'DTB_WC_PUBLIC_READ', true ),
+			'DTB_ENABLE_NONCE_REFRESH'                => self::enabled( 'DTB_ENABLE_NONCE_REFRESH', true ),
+			'DTB_SECURITY_LOGGING'                    => self::enabled( 'DTB_SECURITY_LOGGING', true ),
+			'DTB_ENABLE_PROXY_RATE_LIMIT'             => self::enabled( 'DTB_ENABLE_PROXY_RATE_LIMIT', true ),
+			'DTB_ENABLE_LOGIN_RATE_LIMIT'             => self::enabled( 'DTB_ENABLE_LOGIN_RATE_LIMIT', true ),
+			'DTB_ENABLE_WOO_ADMIN_PAYMENTS_ASSET_GUARD' => self::enabled( 'DTB_ENABLE_WOO_ADMIN_PAYMENTS_ASSET_GUARD', false ),
+			'DTB_ENABLE_WOO_ADMIN_REST_NONCE_COMPAT'  => self::enabled( 'DTB_ENABLE_WOO_ADMIN_REST_NONCE_COMPAT', false ),
 		];
 	}
 }
